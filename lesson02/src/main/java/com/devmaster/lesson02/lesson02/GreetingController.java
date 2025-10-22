4047466d2104f63193e6cae10163c7f8e02802d2
unknown <nguyenvankien12012005@gmail.com>
2025-10-22 11:45:09 +0700

    lesson02

---
+++ lesson02/src/main/java/com/devmaster/lesson02/lesson02/GreetingController.java
@@ -0,0 +1,21 @@
+package com.devmaster.lesson02.lesson02;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private final GreetingService greetingService;
+
+    // Constructor-based Dependency Injection
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return greetingService.greet("Văn Kiên");
+    }
+}
---
+++ lesson02/src/main/java/com/devmaster/lesson02/lesson02/GreetingService.java
@@ -0,0 +1,5 @@
+package com.devmaster.lesson02.lesson02;
+
+public interface GreetingService {
+    String greet(String name);
+}
